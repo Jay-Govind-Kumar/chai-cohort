574a6a874760e54d8ee5aab896f1d7f4a7389a9b
Jay-Govind <govind.iq@gmail.com>
2025-03-05 23:43:00 +0530

    code: code files of machine coding (week 07)

---
+++ Code Files & Challenges/week-07/code/machine_coding.js
@@ -0,0 +1,48 @@
+// Function.prototype.describe = function (description) {
+//   this.description = description;
+//   return this;
+// };
+
+Function.prototype.describe = function () {
+  console.log(`Function name is ${this.name}`);
+};
+// This will print the name of the function
+
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+// greet("Jay"); // Hello Jay
+// greet.describe(); // Function name is greet
+
+/* -------------------------------------------------------------------------- */
+
+// Function Declaration
+function add(a, b) {
+  return a + b;
+}
+
+// Function Expression
+const sum = function (a, b) {
+  return a + b;
+};
+
+// Arrow Function
+// const add = (a, b) => a + b;
+
+/* -------------------------------------------------------------------------- */
+
+// First Class Functions
+function applyOperation(a, b, operation) {
+  return operation(a, b);
+}
+
+const result = applyOperation(3, 2, (a, b) => a - b);
+console.log(result); // 1
+
+/* -------------------------------------------------------------------------- */
+
+// IIFE - Immediately Invoked Function Expression
+(function () {
+  console.log("IIFE");
+})();
